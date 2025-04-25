42d9f4b807b7156c1d5a4207c87e3c99c0731802
Constantine <olexiy2004@gmail.com>
2025-04-25 14:45:12 +0200

    Database

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx'
+import { useDebounce } from 'react-use' 
+import { updateSearchCount } from './appwrite.js'
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -22,6 +24,17 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  //useDebounce(() => setDebouncedSearchTerm(searchTerm), 5000, [searchTerm]);
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    500, // 500ms is usually a good debounce delay for search, 5000ms (5 seconds) might be too long
+    [searchTerm]
+  );
+
 
   const fetchMovies = async (query = '') => {
     setLoading(true);
@@ -40,13 +53,17 @@ const App = () => {
 
         const data = await response.json();
         
-        if (data.Responce === 'False') {
+        if (data.Response === 'False') {
           setErrorMessage(data.Error || 'Failed to fetch movies');
           setMovies([]);
           return;
         }
 
         setMovies(data.results || []);
+
+        if (query && data.results.length > 0) {
+          await updateSearchCount(query, data.results[0]);
+        }
 
     } catch(error) {
       console.error('Error fetching movies:', error);
@@ -58,8 +75,12 @@ const App = () => {
 
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+  if (debouncedSearchTerm) {
+    fetchMovies(debouncedSearchTerm);
+  } else if (debouncedSearchTerm === '') {
+    fetchMovies(''); 
+  }
+}, [debouncedSearchTerm]);
 
 
   return (
